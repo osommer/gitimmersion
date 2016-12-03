373250380ea0f7549599298fec923e9589a794bc
Olivia Sommer <osommer@uncc.edu>
2016-12-03 02:26:36 +0000

    Hello uses Greeter

--- work/lib/hello.rb
+++ work/lib/hello.rb
@@ -1,5 +1,8 @@
+require 'greeter'
+
 # Default is "World"
 # Author: Olivia Sommer (osommer@uncc.edu)
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
